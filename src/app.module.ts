bf885cf9a51d50815b98617318b49b509b8acaf5
Strechtech <jpachacamasimbana@gmail.com>
2026-07-06 13:54:18 -0500

    test: seed de prueba

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SeedModule } from './seed/seed.module';
 
 
 @Module({
@@ -10,6 +11,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     rootPath: join(__dirname, '..', 'public'),
   }), 
   MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
-  PokemonModule],
+  PokemonModule,
+  SeedModule],
 })
 export class AppModule {}
